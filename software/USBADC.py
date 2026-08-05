9faad1af94c191fff13743d44200c4d95bdbeb68
Alexandre Blais <alexandreblais42@gmail.com>
2026-08-05 11:18:37 -0400

    Fixed race condition when writing to pyserial's Serial stream

--- software/USBADC.py
+++ software/USBADC.py
@@ -1,14 +1,17 @@
 from typing import Union
 from enum import Enum
-import zlib
+from zlib import crc32
 import struct
 import random
 from threading import Thread, Lock
 from concurrent.futures import CancelledError, Future
+from queue import Queue
 
 import serial
 import serial.tools.list_ports
 
+class ChecksumDiffers(Exception):
+    pass
 
 class USBADC:
     magic_bytes = b"\xAA\x55\xAA\x55"
@@ -26,7 +29,7 @@ class USBADC:
         class Response(Enum):
             PONG = 0x80
             READ_ADC = 0x81
-            ERROR = 0x83
+            STATUS = 0x83
             VERSION = 0x84
 
     class ADC_Channel(Enum):
@@ -34,6 +37,14 @@ class USBADC:
         USB_SENSE = 1
         TEMPERATURE = 2
         PIN_A3 = 3
+
+    class Status(Enum):
+        OK = 0
+        NOT_AVAILABLE = 1
+        INVALID_DATA = 2
+        TIMEOUT = 3
+        UNKNOWN = 4
+
 
     def __init__(self):
         self.connection = USBADC._get_connection()
@@ -45,6 +56,23 @@ class USBADC:
         self.read_loop_thread = Thread(target=self.read_loop, daemon=True)
         self.read_loop_thread.start()
 
+        self.write_queue = Queue()
+        self.write_thread = Thread(target=self.write_loop, daemon=True)
+        self.write_thread.start()
+
+
+    def write_loop(self):
+        all_data = b""
+        while True:
+            while len(all_data) == 0 or not self.write_queue.empty():
+                all_data += self.write_queue.get()
+
+            amount_written = 0
+            while amount_written != len(all_data):
+                amount_written += self.connection.write(all_data[amount_written:]) or 0
+            all_data = b""
+
+
     def read_loop(self):
         all_bytes = b""
         while True:
@@ -52,12 +80,16 @@ class USBADC:
             # doesn't expose it though.
             all_bytes += self.connection.read(1)
 
-
             # TODO: Avoid doing this at each byte read because it's expensive
             if self.magic_bytes not in all_bytes:
                 continue
 
             # Skip the bytes that aren't part of the magic bytes
+            discarded_bytes = all_bytes[:all_bytes.index(self.magic_bytes)]
+            if discarded_bytes:
+                # print(discarded_bytes)
+                pass
+
             all_bytes = all_bytes[all_bytes.index(self.magic_bytes):]
 
             # Ensure the remaining of the packet is long enough
@@ -76,16 +108,22 @@ class USBADC:
                 continue
 
             data = all_bytes[8:8 + data_length]
+
+            bytes_to_checksum = all_bytes[: 8 + data_length] # All bytes up to the checksum
             checksum = all_bytes[8 + data_length : 8 + 4 + data_length]
 
             # Chop the read bytes for the rest all bytes
             all_bytes = all_bytes[8 + 4 + data_length:]
 
-            print(message_id, message_type, data.hex(), checksum.hex())
-
             with self.futures_lock:
-                future = self.futures.pop(message_id)
-                future.set_result((message_type, data))
+                if message_id not in self.futures:
+                    return
+
+                future: Future = self.futures.pop(message_id)
+                if crc32(bytes_to_checksum).to_bytes(4, "big") != checksum:
+                    future.set_exception(ChecksumDiffers("Received and computed checksums are different"))
+                else:
+                    future.set_result((message_type, data))
 
 
     @staticmethod
@@ -97,6 +135,7 @@ class USBADC:
             usbadc = usbadcs[0]
 
         return serial.Serial(usbadc.device, baudrate=115200)
+
 
     def _get_message_id(self) -> int:
         id = self.next_message_id
@@ -111,13 +150,13 @@ class USBADC:
 
         return id
 
+
     def _send_data(self, data: bytes):
-        checksum = zlib.crc32(data).to_bytes(4, "big")
-        print(f"Sending {data + checksum}")
-        self.connection.write(data + checksum)
+        checksum = crc32(data).to_bytes(4, "big")
+        self.write_queue.put(data + checksum)
+
 
     def _send_command(self, command: Command.Request, data: bytes = b""):
-
         future = Future()
 
         message_id = self._get_message_id()
@@ -134,19 +173,43 @@ class USBADC:
 
         return future.result(timeout=1)
 
+
     def disconnect(self):
         self.connection.close()
 
+
     def ping(self):
         random_payload = random.randbytes(4)
-        self._send_command(self.Command.Request.PING, random_payload)
+        message_type, result_data = self._send_command(self.Command.Request.PING, random_payload)
+
+        match message_type:
+            case self.Command.Response.PONG:
+                if result_data != random_payload:
+                    raise ValueError("PONG payload doesn't match expected payload")
+            case _:
+                raise ValueError("Expected a PONG response from client")
+
 
     def read_adc(self, channel: ADC_Channel) -> float:
         """
         Returns the voltage on the given ADC channel
         """
-        self._send_command(self.Command.Request.READ_ADC, channel.value.to_bytes(1))
-        # TODO: Return read value
+        message_type, result_data = self._send_command(self.Command.Request.READ_ADC, channel.value.to_bytes(1))
+        match message_type:
+            case self.Command.Response.READ_ADC:
+                if len(result_data) != 2:
+                    raise ValueError("Returned data has the wrong size")
+                return int.from_bytes(result_data, "big") / 1000
+            case self.Command.Response.STATUS:
+                if len(result_data) != 1:
+                    raise ValueError("Returned data has the wrong size")
+                status = self.Status(result_data[0])
+                if status != self.Status.OK:
+                    raise ValueError(f"Returned status is not OK: {status}")
+
+            case _:
+                raise ValueError("Expected a READ_ADC or STATUS response")
+
 
     def write_pin(self, port: Union[str, int], pin: int, state: bool):
         """
@@ -166,8 +229,10 @@ class USBADC:
 
         self._send_command(self.Command.Request.WRITE_PIN, struct.pack(">HBB", 1 << pin, port, state))
 
+
     def reboot(self):
         self._send_command(self.Command.Request.REBOOT)
+
 
     def get_firmware_version(self):
         self._send_command(self.Command.Request.VERSION)
--- software/main.py
+++ software/main.py
@@ -4,10 +4,22 @@ from USBADC import USBADC
 
 
 usbadc = USBADC()
+usbadc.ping()
 usbadc.write_pin("GPIOB", 0, False)
 usbadc.write_pin("GPIOB", 0, False)
 usbadc.write_pin("GPIOB", 0, False)
 usbadc.write_pin("GPIOB", 0, False)
-usbadc.read_adc(USBADC.ADC_Channel.USB_SENSE)
 
-time.sleep(10)
+while True:
+    power_meter = usbadc.read_adc(USBADC.ADC_Channel.POWER_METER)
+    usb_sense = usbadc.read_adc(USBADC.ADC_Channel.USB_SENSE)
+    temperature = usbadc.read_adc(USBADC.ADC_Channel.TEMPERATURE)
+    pin_a3 = usbadc.read_adc(USBADC.ADC_Channel.PIN_A3)
+
+    print(f"Power meter: {power_meter}V")
+    print(f"USB Sense: {usb_sense}V")
+    print(f"Temperature: {temperature}V")
+    print(f"Pin A3: {pin_a3}V")
+    print()
+
+    time.sleep(0.1)
